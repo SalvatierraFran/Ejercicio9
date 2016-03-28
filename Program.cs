8e683994bfbbce1cce7c8c0c6ee96d591f3f21b3
SalvatierraFran <franco14_93@hotmail.com>
2016-03-28 00:56:58 -0300

    Programa que crea una piramide sin espacios según el tamaño que se le pida.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ejercicio9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio 09";
+
+            Console.Write("Ingrese el tamaño de la piramide: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int lactual = 1; lactual <= n; lactual++)
+            {
+                for (int asterisco = 0; asterisco < (lactual * 2) - 1; asterisco++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
